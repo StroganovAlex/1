05e23ed8dadd3211c7c5087785781a72944b68c6
StroganovAlex <115008396+StroganovAlex@users.noreply.github.com>
2022-11-11 10:53:51 +0300

    Update script_1.js

--- script_1.js
+++ script_1.js
@@ -1,13 +1,13 @@
 let age = prompt('Привет! Сколько тебе лет?', 10);
-let headText = "<h1 class="header__title">Попробуй еще раз, только на этот раз укажи возраст цифрами";
+let headText = "<h1>Попробуй еще раз, только на этот раз укажи возраст цифрами</h1>";
 if (age <= 20) {  // Тебе меньше 20 лет!;
-    headText = "<h1 class="header__title">Еще совсем зеленый!";
+    headText = "<h1>Еще совсем зеленый!</h1>";
     alert(`Тебе ${age}!`);
 } else if (age <= 50) {  // Тебе в интервале от 20 до 50 лет!; 
-    headText = "<h1 class="header__title">В самом расцвете сил!";
-    alert(`Тебе ${age}!`); 
+    headText = "<h1>В самом расцвете сил!</h1>";
+    alert(`Тебе ${age}!`);
 } else {  // Тебе больше 50 лет!;
-    headText = "<h1 class="header__title">Пора привыкать к земле!";
-    alert(`Тебе ${age}!`); 
+    headText = "<h1>Пора привыкать к земле!</h1>";
+    alert(`Тебе ${age}!`);
 }
 document.body.innerHTML = headText;
