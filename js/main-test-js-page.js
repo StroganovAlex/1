a2363d16c8e74c1b1d4e214043bb4bfb7ffdb421
StroganovAlex <115008396+StroganovAlex@users.noreply.github.com>
2023-01-21 15:18:32 +0300

    Update main-test-js-page.js

--- js/main-test-js-page.js
+++ js/main-test-js-page.js
@@ -19,7 +19,7 @@ document.querySelector('.howOld').addEventListener('click', function() {
 
 function reverse() { //функция обновления содержимого элемента header при нажатии на ссылку "Скрипт №2: ОЛЯ-ЯЛО"
     let headText = '';
-    let str = prompt('Привет! Какое слово перевернуть?', );
+    let str = prompt('Привет! Какое слово перевернуть?', '');
     if (str === null) {  // пользователь отказался вводить слово
       headText = "Привет<br>мир!";
     } else { // пользователь ввел слово которое нужно инвертировать
